0f45ffa4c002fee6e87dfc0f9d938ae237ec827f
Ezra Cerpac <e.cerpac@icloud.com>
2025-08-30 18:39:42 +0200

    Removed more unnecessary dirs

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,21 +2,56 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>j", "*``cgn", { desc = "Search word under cursor and change next match" })
-
-vim.keymap.set("x", "<leader>j", function()
-  -- Yank visual selection to register z
-  vim.cmd('normal! "zy')
-  -- Escape for search
-  local text = vim.fn.getreg("z")
-  text = vim.fn.escape(text, "\\/.*$^~[]")
-  vim.fn.setreg("/", text)
-  -- Exit visual mode and feed 'n' and 'cgn' as keypresses
-  vim.api.nvim_feedkeys("n", "n", false)
-  vim.api.nvim_feedkeys("cgn", "n", false)
-end, { desc = "Change visual selection and repeat with dot" })
+-- vim.keymap.set("n", "<leader>j", "*``cgn", { desc = "Search word under cursor and change next match" })
+--
+-- vim.keymap.set("x", "<leader>j", function()
+--   -- Yank visual selection to register z
+--   vim.cmd('normal! "zy')
+--   -- Escape for search
+--   local text = vim.fn.getreg("z")
+--   text = vim.fn.escape(text, "\\/.*$^~[]")
+--   vim.fn.setreg("/", text)
+--   -- Exit visual mode and feed 'n' and 'cgn' as keypresses
+--   vim.api.nvim_feedkeys("n", "n", false)
+--   vim.api.nvim_feedkeys("cgn", "n", false)
+-- end, { desc = "Change visual selection and repeat with dot" })
 
 -- Ensure <leader>e opens mini.files (override any LazyVim defaults)
 vim.keymap.set("n", "<leader>e", function()
   require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
 end, { desc = "Open mini.files (Directory of Current File)" })
+
+require("fzf-lua").setup({
+  files = {
+    actions = {
+      -- remap toggle hidden from Alt-h to Ctrl-h
+      ["ctrl-h"] = require("fzf-lua.actions").toggle_hidden,
+
+      ["default"] = function(selected)
+        if not selected or #selected == 0 then
+          return
+        end
+        local file = selected[1]
+
+        -- 1. Strip nerd font icon + whitespace if present
+        file = file:gsub("^%s*[\u{e000}-\u{f8ff}]%s*", "")
+
+        -- 2. Expand relative paths like `.config/...` → `$HOME/.config/...`
+        if not file:match("^/") then
+          file = vim.fn.expand("~/" .. file)
+        end
+
+        -- 3. Map chezmoi source dir paths → target paths
+        local chezmoi_dir = vim.fn.expand("~/.local/share/chezmoi/")
+        if file:sub(1, #chezmoi_dir) == chezmoi_dir then
+          file = file:gsub(chezmoi_dir, vim.fn.expand("~") .. "/")
+          file = file:gsub("^dot_", ".") -- dotfile convention
+          file = file:gsub("private_", "") -- strip private_ prefix
+          file = file:gsub("slash", "/") -- chezmoi escape convention
+        end
+
+        vim.cmd("ChezmoiEdit " .. vim.fn.fnameescape(file))
+      end,
+    },
+  },
+})
--- dot_config/sketchybar/sketchybar/plugins/media.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  sketchybar --set "$NAME" label="$MEDIA" drawing=on icon.color=0xffffffff label.color=0xffffffff background.drawing=on
-else
-  sketchybar --set "$NAME" drawing=off background.drawing=off
-fi
