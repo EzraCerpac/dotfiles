815a600f9f2173d1d719ea09f06bcd4ff236291b
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-09-02 00:59:54 +0200

    Update README.md Update .config/nvim/lua/plugins/ai.lua Remove .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/mini-move.lua Add .config/nvim/lua/unused/better-escape.lua Update .config/opencode/opencode.json Update .config/jj/config.toml Add .gitignore Update .markdownlint-cli2.yaml Change attributes of .config/nvim/lua/unused/modules/jj_fzf.lua Change attributes of .config/nvim/lua/unused/modules/jjui.lua Change attributes of .config/nvim/lua/unused/zellij.lua

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -17,4 +17,42 @@ return {
       end
     end, { desc = "Clear Copilot suggestion or fallback" }),
   },
+  {
+    -- https://github.com/aweis89/ai-terminals.nvim
+    "aweis89/ai-terminals.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    opts = {
+      auto_terminal_keymaps = {
+        prefix = "<leader>a",
+        terminals = {
+          { name = "claude", key = "a" },
+          { name = "opencode", key = "o" },
+          { name = "codex", key = "c" },
+        },
+      },
+    },
+  },
+  -- {
+  --   "NickvanDyke/opencode.nvim",
+  --   dependencies = {
+  --     "folke/snacks.nvim",
+  --   },
+  --   ---@type opencode.Config
+  --   opts = {
+  --     -- You can add custom prompts or contexts here later, e.g.:
+  --     -- prompts = { },
+  --     -- contexts = { },
+  --   },
+  -- -- stylua: ignore
+  -- keys = {
+  --   { "<leader>Ot", function() require("opencode").toggle() end, desc = "Toggle embedded opencode" },
+  --   { "<leader>Oa", function() require("opencode").ask("@cursor: ") end, desc = "Ask opencode", mode = "n" },
+  --   { "<leader>Oa", function() require("opencode").ask("@selection: ") end, desc = "Ask opencode about selection", mode = "v" },
+  --   { "<leader>Op", function() require("opencode").select_prompt() end, desc = "Select prompt", mode = { "n", "v" } },
+  --   { "<leader>On", function() require("opencode").command("session_new") end, desc = "New session" },
+  --   { "<leader>Oy", function() require("opencode").command("messages_copy") end, desc = "Copy last message" },
+  --   { "<S-C-u>",    function() require("opencode").command("messages_half_page_up") end,   desc = "Scroll messages up" },
+  --   { "<S-C-d>",    function() require("opencode").command("messages_half_page_down") end, desc = "Scroll messages down" },
+  -- },
+  -- },
 }
---
+++ dot_config/nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,17 @@
+return {
+  "echasnovski/mini.move",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      up = "<C-M-S-k",
+      down = "<C-M-S-j>",
+      left = "<C-M-S-h>",
+      right = "<C-M-S-l>",
+
+      line_up = "<C-M-S-k>",
+      line_down = "<C-M-S-j>",
+      line_left = "<C-M-S-h>",
+      line_right = "<C-M-S-l>",
+    },
+  },
+}
