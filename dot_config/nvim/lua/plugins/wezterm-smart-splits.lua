bec142a13f183af408aac33f5cbcd56f16f3edda
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-09-15 17:20:50 +0200

    Add wezterm with aerospace and neovim integration
    Update .config/aerospace/aerospace.toml
    Add .config/aerospace/bin/wezterm-smart-focus
    Add .config/nvim/lua/plugins/wezterm-smart-splits.lua
    Add .config/wezterm/wezterm.lua

---
+++ dot_config/nvim/lua/plugins/wezterm-smart-splits.lua
@@ -0,0 +1,23 @@
+---@type LazySpec
+return {
+  "mrjones2014/smart-splits.nvim",
+  lazy = false, -- ensure user-var is set for WezTerm immediately
+  config = function()
+    require("smart-splits").setup({
+      log_level = "warn",
+      multiplexer_integration = "wezterm",
+      at_edge = "wrap", -- or "stop" if you prefer no wrap-around
+    })
+
+    local ss = require("smart-splits")
+    local map = vim.keymap.set
+    local opts = { silent = true, noremap = true }
+
+    -- Alt/meta + hjkl to move between splits; at edge, fall through to WezTerm pane
+    map("n", "<A-h>", ss.move_cursor_left, vim.tbl_extend("force", opts, { desc = "Move left (smart-splits)" }))
+    map("n", "<A-j>", ss.move_cursor_down, vim.tbl_extend("force", opts, { desc = "Move down (smart-splits)" }))
+    map("n", "<A-k>", ss.move_cursor_up, vim.tbl_extend("force", opts, { desc = "Move up (smart-splits)" }))
+    map("n", "<A-l>", ss.move_cursor_right, vim.tbl_extend("force", opts, { desc = "Move right (smart-splits)" }))
+  end,
+}
+
