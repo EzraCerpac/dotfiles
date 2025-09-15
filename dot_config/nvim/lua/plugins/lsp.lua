26daff097fc7e238dddf90ca4ae6bc6df5b60c21
Ezra Cerpac <e.cerpac@icloud.com>
2025-09-15 20:31:03 +0200

    Add wezterm type hinting in nvim
    Update .wezterm.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,17 @@
 return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    dependencies = {
+      { "DrKJeff16/wezterm-types", lazy = true },
+    },
+    opts = {
+      library = {
+        -- Other library configs...
+        { path = "wezterm-types", mods = { "wezterm" } },
+      },
+    },
+  },
   -- {
   --   "neovim/nvim-lspconfig",
   --   opts = {
