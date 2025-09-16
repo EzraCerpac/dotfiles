dc38d92b701e560aa0be50acf912754a91e5578d
Ezra Cerpac <e.cerpac@icloud.com>
2025-09-16 14:22:05 +0200

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -13,11 +13,6 @@ vim.keymap.del("n", "<leader><tab>d")
 vim.keymap.del("n", "<leader><tab>[")
 -- Space
 vim.keymap.del("n", "<leader><space>")
--- Move commands
--- vim.keymap.del({ "n", "v" }, "M-h")
-vim.keymap.del({ "n", "v" }, "M-j")
-vim.keymap.del({ "n", "v" }, "M-k")
--- vim.keymap.del({ "n", "v" }, "M-l")
 
 -- vim.keymap.set("n", "<leader>j", "*``cgn", { desc = "Search word under cursor and change next match" })
 --
