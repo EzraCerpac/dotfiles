32ae8d1c57f2a8f55e6831777143d06580e1167a
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-09-18 17:42:58 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -24,6 +24,28 @@ config.hide_mouse_cursor_when_typing = true
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 
+wezterm.on("update-right-status", function(window, _)
+  local SOLID_LEFT_ARROW = ""
+  local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+  local prefix = ""
+
+  -- if window:leader_is_active() then
+  --   prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+  --   SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  -- end
+
+  if window:active_tab():tab_id() ~= 0 then
+    ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+  end -- arrow color based on if tab is first pane
+
+  window:set_left_status(wezterm.format({
+    { Background = { Color = "#b7bdf8" } },
+    { Text = prefix },
+    ARROW_FOREGROUND,
+    { Text = SOLID_LEFT_ARROW },
+  }))
+end)
+
 local function is_vim(pane)
   -- this is set by the plugin, and unset on ExitPre in Neovim
   return pane:get_user_vars().IS_NVIM == "true"
