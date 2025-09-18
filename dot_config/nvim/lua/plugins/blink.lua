5f1c43c9a659f97a05e041bd706e63997a2c606f
Ezra Cerpac <e.cerpac@icloud.com>
2025-09-18 16:42:38 +0200

    Update .config/nvim/lua/plugins/blink.lua

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,201 +1,201 @@
--- Trying blink.cmp as an alternative to nvim.cmp. So far:
+-- -- Trying blink.cmp as an alternative to nvim.cmp. So far:
+-- --
+-- -- - It's a little faster.
+-- -- - Had to turn off snippets immediately.
+-- -- - Turned off ghost text.
+-- -- - It's autocompleting a little too eagerly - writing a JSDoc
+-- --   comment like /** triggers autocompletion on the last *
+-- return {
+--   "saghen/blink.cmp",
+--   version = not vim.g.lazyvim_blink_main and "*",
+--   build = vim.g.lazyvim_blink_main and "cargo build --release",
 --
--- - It's a little faster.
--- - Had to turn off snippets immediately.
--- - Turned off ghost text.
--- - It's autocompleting a little too eagerly - writing a JSDoc
---   comment like /** triggers autocompletion on the last *
-return {
-  "saghen/blink.cmp",
-  version = not vim.g.lazyvim_blink_main and "*",
-  build = vim.g.lazyvim_blink_main and "cargo build --release",
-
-  opts_extend = {
-    "sources.completion.enabled_providers",
-    "sources.compat",
-    "sources.default",
-  },
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    "Kaiser-Yang/blink-cmp-git",
-    "fang2hou/blink-copilot",
-    -- add blink.compat to dependencies
-    {
-      "saghen/blink.compat",
-      optional = true, -- make optional so it's only enabled if any extras need it
-      opts = {},
-      version = not vim.g.lazyvim_blink_main and "*",
-    },
-  },
-  event = "InsertEnter",
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    enabled = function()
-      -- Disable on Markdown files
-      return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
-        and vim.bo.buftype ~= "prompt"
-        and vim.b.completion ~= false
-    end,
-    snippets = nil,
-    -- snippets = {
-    --   expand = function(snippet, _)
-    --     return LazyVim.cmp.expand(snippet)
-    --   end,
-    -- },
-    appearance = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = false,
-      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
-    },
-    completion = {
-      accept = {
-        -- experimental auto-brackets support
-        auto_brackets = {
-          enabled = true,
-        },
-      },
-      menu = {
-        draw = {
-          treesitter = { "lsp" },
-        },
-      },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 200,
-        draw = function(opts)
-          if opts.item and opts.item.documentation then
-            local out = require("pretty_hover.parser").parse(opts.item.documentation.value)
-            opts.item.documentation.value = out:string()
-          end
-
-          opts.default_implementation(opts)
-        end,
-      },
-      ghost_text = {
-        enabled = false, -- vim.g.ai_cmp,
-      },
-    },
-
-    -- experimental signature help support
-    -- signature = { enabled = true },
-
-    sources = {
-      -- adding any nvim-cmp sources here will enable them
-      -- with blink.compat
-      compat = {},
-      default = {
-        "git",
-        "lsp",
-        "path",
-        "snippets",
-        "buffer",
-      },
-      providers = {
-        git = {
-          module = "blink-cmp-git",
-          name = "Git",
-          opts = {
-            -- options for the blink-cmp-git
-          },
-        },
-        copilot = {
-          module = "blink-copilot",
-        },
-      },
-    },
-    cmdline = { enabled = false },
-
-    keymap = {
-      preset = "super-tab",
-      ["<C-y>"] = { "select_and_accept" },
-      ["<Tab>"] = {
-        function(cmp)
-          if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-            cmp.hide()
-            return (
-              require("copilot-lsp.nes").apply_pending_nes()
-              and require("copilot-lsp.nes").walk_cursor_end_edit()
-            )
-          end
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        "snippet_forward",
-        "fallback",
-      },
-    },
-  },
-  ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
-  config = function(_, opts)
-    -- setup compat sources
-    local enabled = opts.sources.default
-    for _, source in ipairs(opts.sources.compat or {}) do
-      opts.sources.providers[source] = vim.tbl_deep_extend(
-        "force",
-        { name = source, module = "blink.compat.source" },
-        opts.sources.providers[source] or {}
-      )
-      if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-        table.insert(enabled, source)
-      end
-    end
-
-    -- add ai_accept to <Tab> key
-    -- if not opts.keymap["<Tab>"] then
-    --   if opts.keymap.preset == "super-tab" then -- super-tab
-    --     opts.keymap["<Tab>"] = {
-    --       require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-    --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-    --       "fallback",
-    --     }
-    --   else -- other presets
-    --     opts.keymap["<Tab>"] = {
-    --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-    --       "fallback",
-    --     }
-    --   end
-    -- end
-
-    -- Unset custom prop to pass blink.cmp validation
-    opts.sources.compat = nil
-
-    -- check if we need to override symbol kinds
-    for _, provider in pairs(opts.sources.providers or {}) do
-      ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
-      if provider.kind then
-        local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-        local kind_idx = #CompletionItemKind + 1
-
-        CompletionItemKind[kind_idx] = provider.kind
-        ---@diagnostic disable-next-line: no-unknown
-        CompletionItemKind[provider.kind] = kind_idx
-
-        ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
-        local transform_items = provider.transform_items
-        ---@param ctx blink.cmp.Context
-        ---@param items blink.cmp.CompletionItem[]
-        provider.transform_items = function(ctx, items)
-          items = transform_items and transform_items(ctx, items) or items
-          for _, item in ipairs(items) do
-            item.kind = kind_idx or item.kind
-          end
-          return items
-        end
-
-        -- Unset custom prop to pass blink.cmp validation
-        provider.kind = nil
-      end
-    end
-
-    require("blink.cmp").setup(opts)
-  end,
-}
+--   opts_extend = {
+--     "sources.completion.enabled_providers",
+--     "sources.compat",
+--     "sources.default",
+--   },
+--   dependencies = {
+--     "rafamadriz/friendly-snippets",
+--     "Kaiser-Yang/blink-cmp-git",
+--     "fang2hou/blink-copilot",
+--     -- add blink.compat to dependencies
+--     {
+--       "saghen/blink.compat",
+--       optional = true, -- make optional so it's only enabled if any extras need it
+--       opts = {},
+--       version = not vim.g.lazyvim_blink_main and "*",
+--     },
+--   },
+--   event = "InsertEnter",
+--
+--   ---@module 'blink.cmp'
+--   ---@type blink.cmp.Config
+--   opts = {
+--     enabled = function()
+--       -- Disable on Markdown files
+--       return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+--         and vim.bo.buftype ~= "prompt"
+--         and vim.b.completion ~= false
+--     end,
+--     snippets = nil,
+--     -- snippets = {
+--     --   expand = function(snippet, _)
+--     --     return LazyVim.cmp.expand(snippet)
+--     --   end,
+--     -- },
+--     appearance = {
+--       -- sets the fallback highlight groups to nvim-cmp's highlight groups
+--       -- useful for when your theme doesn't support blink.cmp
+--       -- will be removed in a future release, assuming themes add support
+--       use_nvim_cmp_as_default = false,
+--       -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+--       -- adjusts spacing to ensure icons are aligned
+--       nerd_font_variant = "mono",
+--     },
+--     completion = {
+--       accept = {
+--         -- experimental auto-brackets support
+--         auto_brackets = {
+--           enabled = true,
+--         },
+--       },
+--       menu = {
+--         draw = {
+--           treesitter = { "lsp" },
+--         },
+--       },
+--       documentation = {
+--         auto_show = true,
+--         auto_show_delay_ms = 200,
+--         draw = function(opts)
+--           if opts.item and opts.item.documentation then
+--             local out = require("pretty_hover.parser").parse(opts.item.documentation.value)
+--             opts.item.documentation.value = out:string()
+--           end
+--
+--           opts.default_implementation(opts)
+--         end,
+--       },
+--       ghost_text = {
+--         enabled = false, -- vim.g.ai_cmp,
+--       },
+--     },
+--
+--     -- experimental signature help support
+--     -- signature = { enabled = true },
+--
+--     sources = {
+--       -- adding any nvim-cmp sources here will enable them
+--       -- with blink.compat
+--       compat = {},
+--       default = {
+--         "git",
+--         "lsp",
+--         "path",
+--         "snippets",
+--         "buffer",
+--       },
+--       providers = {
+--         git = {
+--           module = "blink-cmp-git",
+--           name = "Git",
+--           opts = {
+--             -- options for the blink-cmp-git
+--           },
+--         },
+--         copilot = {
+--           module = "blink-copilot",
+--         },
+--       },
+--     },
+--     cmdline = { enabled = false },
+--
+--     keymap = {
+--       preset = "super-tab",
+--       ["<C-y>"] = { "select_and_accept" },
+--       ["<Tab>"] = {
+--         function(cmp)
+--           if vim.b[vim.api.nvim_get_current_buf()].nes_state then
+--             cmp.hide()
+--             return (
+--               require("copilot-lsp.nes").apply_pending_nes()
+--               and require("copilot-lsp.nes").walk_cursor_end_edit()
+--             )
+--           end
+--           if cmp.snippet_active() then
+--             return cmp.accept()
+--           else
+--             return cmp.select_and_accept()
+--           end
+--         end,
+--         "snippet_forward",
+--         "fallback",
+--       },
+--     },
+--   },
+--   ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+--   config = function(_, opts)
+--     -- setup compat sources
+--     local enabled = opts.sources.default
+--     for _, source in ipairs(opts.sources.compat or {}) do
+--       opts.sources.providers[source] = vim.tbl_deep_extend(
+--         "force",
+--         { name = source, module = "blink.compat.source" },
+--         opts.sources.providers[source] or {}
+--       )
+--       if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+--         table.insert(enabled, source)
+--       end
+--     end
+--
+--     -- add ai_accept to <Tab> key
+--     -- if not opts.keymap["<Tab>"] then
+--     --   if opts.keymap.preset == "super-tab" then -- super-tab
+--     --     opts.keymap["<Tab>"] = {
+--     --       require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+--     --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+--     --       "fallback",
+--     --     }
+--     --   else -- other presets
+--     --     opts.keymap["<Tab>"] = {
+--     --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+--     --       "fallback",
+--     --     }
+--     --   end
+--     -- end
+--
+--     -- Unset custom prop to pass blink.cmp validation
+--     opts.sources.compat = nil
+--
+--     -- check if we need to override symbol kinds
+--     for _, provider in pairs(opts.sources.providers or {}) do
+--       ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+--       if provider.kind then
+--         local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+--         local kind_idx = #CompletionItemKind + 1
+--
+--         CompletionItemKind[kind_idx] = provider.kind
+--         ---@diagnostic disable-next-line: no-unknown
+--         CompletionItemKind[provider.kind] = kind_idx
+--
+--         ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+--         local transform_items = provider.transform_items
+--         ---@param ctx blink.cmp.Context
+--         ---@param items blink.cmp.CompletionItem[]
+--         provider.transform_items = function(ctx, items)
+--           items = transform_items and transform_items(ctx, items) or items
+--           for _, item in ipairs(items) do
+--             item.kind = kind_idx or item.kind
+--           end
+--           return items
+--         end
+--
+--         -- Unset custom prop to pass blink.cmp validation
+--         provider.kind = nil
+--       end
+--     end
+--
+--     require("blink.cmp").setup(opts)
+--   end,
+-- }
