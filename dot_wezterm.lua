aedac614c7d22e003dd7bec1799c2d41f3a8ab8b
Ezra Cerpac <e.cerpac@icloud.com>
2025-09-18 18:02:25 +0200

    Update .config/fish/fish_variables Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -31,32 +31,7 @@ bar.apply_to_config(config, {
   hostname = { enabled = false },
   clock = { enabled = false },
 })
-config.hide_tab_bar_if_only_one_tab = false
--- config.tab_bar_at_bottom = true
--- config.use_fancy_tab_bar = false
--- config.tab_and_split_indices_are_zero_based = true
-
--- wezterm.on("update-right-status", function(window, _)
---   local SOLID_LEFT_ARROW = ""
---   local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
---   local prefix = ""
---
---   -- if window:leader_is_active() then
---   --   prefix = " " .. utf8.char(0x1f30a) -- ocean wave
---   --   SOLID_LEFT_ARROW = utf8.char(0xe0b2)
---   -- end
---
---   if window:active_tab():tab_id() ~= 0 then
---     ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
---   end -- arrow color based on if tab is first pane
---
---   window:set_left_status(wezterm.format({
---     { Background = { Color = "#b7bdf8" } },
---     { Text = prefix },
---     ARROW_FOREGROUND,
---     { Text = SOLID_LEFT_ARROW },
---   }))
--- end)
+config.hide_tab_bar_if_only_one_tab = true
 
 local function is_vim(pane)
   -- this is set by the plugin, and unset on ExitPre in Neovim
