20f4f43b49a7212c17873048659497eecbd8ffbf
Ezra Cerpac <e.cerpac@icloud.com>
2025-10-07 10:21:18 +0200

    Update .config/nvim/lua/plugins/ai.lua Update .gitignore

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -77,47 +77,6 @@ return {
               show_result_in_chat = true,
             },
           },
-          ---@module "vectorcode"
-          vectorcode = {
-            ---@type VectorCode.CodeCompanion.ExtensionOpts
-            opts = {
-              tool_group = {
-                -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
-                enabled = true,
-                -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
-                -- if you use @vectorcode_vectorise, it'll be very handy to include
-                -- `file_search` here.
-                extras = {},
-                collapse = false, -- whether the individual tools should be shown in the chat
-              },
-              tool_opts = {
-                ---@type VectorCode.CodeCompanion.ToolOpts
-                ["*"] = {},
-                ---@type VectorCode.CodeCompanion.LsToolOpts
-                ls = {},
-                ---@type VectorCode.CodeCompanion.VectoriseToolOpts
-                vectorise = {},
-                ---@type VectorCode.CodeCompanion.QueryToolOpts
-                query = {
-                  max_num = { chunk = -1, document = -1 },
-                  default_num = { chunk = 50, document = 10 },
-                  include_stderr = false,
-                  use_lsp = false,
-                  no_duplicate = true,
-                  chunk_mode = false,
-                  ---@type VectorCode.CodeCompanion.SummariseOpts
-                  summarise = {
-                    ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
-                    enabled = false,
-                    adapter = nil,
-                    query_augmented = true,
-                  },
-                },
-                files_ls = {},
-                files_rm = {},
-              },
-            },
-          },
         },
       })
       vim.keymap.set(
@@ -166,12 +125,6 @@ return {
     config = function()
       require("mcphub").setup()
     end,
-  },
-  {
-    "Davidyz/VectorCode",
-    version = "*", -- optional, depending on whether you're on nightly or release
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "VectorCode", -- if you're lazy-loading VectorCode
   },
   -- {
   --   -- https://github.com/aweis89/ai-terminals.nvim
