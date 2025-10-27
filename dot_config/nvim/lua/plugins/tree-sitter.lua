62f7f25b1504183be32ba40c64afdf26655ac8d7
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-10-27 16:22:19 +0100

    Update .config/nvim/lua/plugins/tree-sitter.lua

--- dot_config/nvim/lua/plugins/tree-sitter.lua
+++ dot_config/nvim/lua/plugins/tree-sitter.lua
@@ -1,22 +1,32 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     opts = {
       ensure_installed = {
         "latex",
         "typst",
       },
-      textobjects = {
-        swap = {
-          enable = true,
-          swap_next = {
-            ["ga"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["gA"] = "@parameter.inner",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          swap = {
+            enable = true,
+            swap_next = {
+              ["ga"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["gA"] = "@parameter.inner",
+            },
           },
         },
-      },
-    },
+      })
+    end,
   },
 }
