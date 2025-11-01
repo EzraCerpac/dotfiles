e453929109a193e0afb48a0e7c5ff6e69d308261
Ezra Cerpac <e.cerpac@icloud.com>
2025-11-01 15:22:14 +0100

    Update .config/nvim/lua/plugins/ai.lua

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -127,7 +127,28 @@ return {
   {
     "azorng/goose.nvim",
     config = function()
-      require("goose").setup({})
+      require("goose").setup({
+        keymap = {
+    global = {
+      toggle = '<leader>gg',                 -- Open goose. Close if opened
+      open_input = '<leader>gi',             -- Opens and focuses on input window on insert mode
+      open_input_new_session = '<leader>gI', -- Opens and focuses on input window on insert mode. Creates a new session
+      open_output = '<leader>go',            -- Opens and focuses on output window
+      toggle_focus = '<leader>gt',           -- Toggle focus between goose and last window
+      close = '<leader>gq',                  -- Close UI windows
+      toggle_fullscreen = '<leader>gf',      -- Toggle between normal and fullscreen mode
+      select_session = '<leader>gs',         -- Select and load a goose session
+      goose_mode_chat = '<leader>gmc',       -- Set goose mode to `chat`. (Tool calling disabled. No editor context besides selections)
+      goose_mode_auto = '<leader>gma',       -- Set goose mode to `auto`. (Default mode with full agent capabilities)
+      configure_provider = '<leader>gp',     -- Quick provider and model switch from predefined list
+      diff_open = '<leader>gd',              -- Opens a diff tab of a modified file since the last goose prompt
+      diff_next = '<leader>g]',              -- Navigate to next file diff
+      diff_prev = '<leader>g[',              -- Navigate to previous file diff
+      diff_close = '<leader>gc',             -- Close diff view tab and return to normal editing
+      diff_revert_all = '<leader>gra',       -- Revert all file changes since the last goose prompt
+      diff_revert_this = '<leader>grt',      -- Revert current file changes since the last goose prompt
+    },
+      })
     end,
     dependencies = {
       "nvim-lua/plenary.nvim",
