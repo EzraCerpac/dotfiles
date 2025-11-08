830463a65a21f47ff5cd5ac397b8fd9bce08f46a
Ezra Cerpac <e.cerpac@icloud.com>
2025-11-08 18:36:37 +0100

    Update .config/nvim/lua/plugins/tabout.lua

--- dot_config/nvim/lua/plugins/tabout.lua
+++ dot_config/nvim/lua/plugins/tabout.lua
@@ -1,9 +1,9 @@
 return {
   {
     "abecodes/tabout.nvim",
-    lazy = false,
+    -- lazy = false,
     event = "InsertEnter",
-    priority = 1000,
+    -- priority = 1000,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "L3MON4D3/LuaSnip",
