a247b7815f9c04148fc94576bd6e0f1012beedf4
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-11-16 20:07:49 +0100

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -43,6 +43,14 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "wincmd L",
 })
 
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
 -- Ensures that when exiting NeoVim, Zellij returns to normal mode
 -- vim.api.nvim_create_autocmd("VimLeave", {
 --   pattern = "*",
