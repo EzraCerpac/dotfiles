80131e64f60ff30e52595b966fc51a37e572be4d
Ezra Cerpac <e.cerpac@icloud.com>
2025-11-20 17:33:12 +0100

    Update .config/nvim/lua/plugins/project.lua

--- dot_config/nvim/lua/plugins/project.lua
+++ dot_config/nvim/lua/plugins/project.lua
@@ -1,9 +1,10 @@
+local manual_override = vim.g.project_manual_mode_override
 return {
   -- https://github.com/ahmedkhalf/project.nvim
   "ahmedkhalf/project.nvim",
   config = function()
     require("project_nvim").setup({
-      manual_mode = vim.g.project_manual_mode_override or false,
+      manual_mode = (manual_override == true) or (manual_override == 1) or false,
     })
   end,
 }
