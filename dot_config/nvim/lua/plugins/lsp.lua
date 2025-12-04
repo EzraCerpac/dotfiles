dd2fb9431140446c23527877587a90a5613d9498
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-12-04 12:48:43 +0100

    Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -30,4 +30,51 @@ return {
       },
     },
   },
+  -- Julia DAP configuration
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+
+      -- Julia debug adapter using DebugAdapter.jl
+      -- Requires: Pkg.add("DebugAdapter") in Julia
+      dap.adapters.julia = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "julia",
+          args = {
+            "--startup-file=no",
+            "--history-file=no",
+            "-e",
+            [[
+              using DebugAdapter
+              DebugAdapter.run_debugger(; port=]] .. "${port}" .. [[)
+            ]],
+          },
+        },
+      }
+
+      dap.configurations.julia = {
+        {
+          type = "julia",
+          request = "launch",
+          name = "Launch Julia file",
+          program = "${file}",
+          projectDir = "${workspaceFolder}",
+          juliaEnv = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+        {
+          type = "julia",
+          request = "launch",
+          name = "Launch Julia file (stop on entry)",
+          program = "${file}",
+          projectDir = "${workspaceFolder}",
+          juliaEnv = "${workspaceFolder}",
+          stopOnEntry = true,
+        },
+      }
+    end,
+  },
 }
