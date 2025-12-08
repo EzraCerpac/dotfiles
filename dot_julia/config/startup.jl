6749047a92aadbc79370ecf316c94af099605174
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-12-08 21:23:26 +0100

    Update .julia/config/startup.jl
    Update .config/fish/config.fish
    Update .julia/config/startup.jl
    Add .julia/dev/MyTools/src/MyTools.jl
    
    Update .config/fish/config.fish
    Update .julia/config/startup.jl
    
    Remove .julia/dev/MyTools/src/MyTools.jl
    
    Update .julia/config/startup.jl
    
    Update .julia/config/startup.jl

--- dot_julia/config/startup.jl
+++ dot_julia/config/startup.jl
@@ -1,8 +1,34 @@
+global_env = joinpath(DEPOT_PATH[1], "environments", "v$(VERSION.major).$(VERSION.minor)")
+if isdir(global_env)
+    push!(LOAD_PATH, global_env)  # append instead of prepend
+end
+
+using Pkg
+atreplinit() do repl
+    try
+        @eval using OhMyREPL
+    catch e
+        @warn "Error initializing OhMyRepl in startup.jl" exception = (e, catch_backtrace())
+    end
+end
+
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise in startup.jl" exception = (e, catch_backtrace())
+end
+
+try
+    using BenchmarkTools
+catch e
+    @warn "Error initializing BenchmarkTools in startup.jl" exception = (e, catch_backtrace())
+end
+
 atreplinit() do repl
     InteractiveUtils.define_editor("zed") do cmd, path, line, column
         `$cmd $path:$line:$column`
     end
-end# using Revise
+end
 
 ENV["PLUTO_USER_JS"] = expanduser("~/.julia/config/pluto_user.js")
 
