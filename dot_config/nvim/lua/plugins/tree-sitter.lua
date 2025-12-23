7da5d17ef250574882fc66612b1b2b6448a5cef6
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-23 21:03:50 +0100

    Update .config/nvim/lua/plugins/tree-sitter.lua

--- dot_config/nvim/lua/plugins/tree-sitter.lua
+++ dot_config/nvim/lua/plugins/tree-sitter.lua
@@ -3,20 +3,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
-
-      -- Provides `queries/wgsl/*` for highlighting, etc.
-      "szebniok/tree-sitter-wgsl",
     },
     opts = function(_, opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.wgsl = vim.tbl_deep_extend("force", parser_config.wgsl or {}, {
-        install_info = {
-          url = "https://github.com/szebniok/tree-sitter-wgsl",
-          files = { "src/parser.c", "src/scanner.c" },
-        },
-        filetype = "wgsl",
-      })
-
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
         "latex",
