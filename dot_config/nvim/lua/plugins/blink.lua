7099506892e7f25561aa427a80fe77bad865c81c
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-24 01:57:44 +0100

    Update .config/nvim/lua/plugins/blink.lua

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -9,19 +9,15 @@ return {
         ["<Tab>"] = false,
         ["<S-Tab>"] = false,
       },
-      completion = {
-        trigger = {
-          show_in_snippet = false,
-          show_on_insert_on_trigger_character = function(ctx)
-            local bufname = vim.api.nvim_buf_get_name(ctx.bufnr)
-            -- In codecompanion buffers, don't trigger on backslash
-            if bufname:match("%[CodeCompanion%]") and ctx.char == "\\" then
-              return false
-            end
-            return true
-          end,
+      sources = {
+        per_filetype = {
+          codecompanion = {
+            "codecompanion",
+            "buffer",
+          },
         },
       },
     },
   },
 }
+
