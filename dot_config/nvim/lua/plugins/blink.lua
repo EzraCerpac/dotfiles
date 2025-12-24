a98a006c7f5ed42fc6063d6c738e9c89d6bbe42a
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-24 01:45:46 +0100

    Update .config/nvim/lua/plugins/blink.lua

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -3,19 +3,24 @@ return {
     "saghen/blink.cmp",
     optional = true,
     opts = {
-      enabled = function()
-        local bufname = vim.api.nvim_buf_get_name(0)
-        -- Disable in codecompanion buffers to allow slash commands
-        if bufname:match("%[CodeCompanion%]") then
-          return false
-        end
-        return vim.bo.buftype ~= "prompt" and vim.b.completion ~= false
-      end,
       keymap = {
         preset = "enter",
         -- Tab is reserved for sidekick NES + tabout.nvim; accept completions with <C-y>
         ["<Tab>"] = false,
         ["<S-Tab>"] = false,
+      },
+      completion = {
+        trigger = {
+          show_in_snippet = false,
+          show_on_insert_on_trigger_character = function(ctx)
+            local bufname = vim.api.nvim_buf_get_name(ctx.bufnr)
+            -- In codecompanion buffers, don't trigger on backslash
+            if bufname:match("%[CodeCompanion%]") and ctx.char == "\\" then
+              return false
+            end
+            return true
+          end,
+        },
       },
     },
   },
