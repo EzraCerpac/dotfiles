2b001d4fa638ced5e06a9239e4959ab526ac35ff
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-24 01:43:53 +0100

    Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/plugins/blink.lua

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -3,6 +3,14 @@ return {
     "saghen/blink.cmp",
     optional = true,
     opts = {
+      enabled = function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        -- Disable in codecompanion buffers to allow slash commands
+        if bufname:match("%[CodeCompanion%]") then
+          return false
+        end
+        return vim.bo.buftype ~= "prompt" and vim.b.completion ~= false
+      end,
       keymap = {
         preset = "enter",
         -- Tab is reserved for sidekick NES + tabout.nvim; accept completions with <C-y>
