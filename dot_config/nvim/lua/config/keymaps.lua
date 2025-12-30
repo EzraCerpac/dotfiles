69f9fdd045c64a49020ddcd64c285f852f4a22e6
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-30 13:38:50 +0100

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/explorer.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -61,11 +61,11 @@ vim.api.nvim_create_user_command("RTFHighlight", function(args)
   end
   local line1, line2 = args.line1, args.line2
   local content = table.concat(vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false), "\n")
+  vim.notify("content lines: " .. #content, vim.log.levels.INFO)
   local lexer = vim.bo.filetype ~= "" and vim.bo.filetype or "lua"
-  vim.fn.system("sh " .. vim.fn.shellescape(script) .. " " .. lexer, content)
-  if vim.v.shell_error ~= 0 then
-    vim.notify("clipboard script failed", vim.log.levels.ERROR)
-    return
-  end
+  vim.notify("lexer: " .. lexer, vim.log.levels.INFO)
+  local result = vim.fn.system("sh " .. vim.fn.shellescape(script) .. " " .. lexer, content)
+  vim.notify("result: " .. result, vim.log.levels.INFO)
+  vim.notify("shell_error: " .. vim.v.shell_error, vim.log.levels.INFO)
   vim.notify("RTF copied to clipboard", vim.log.levels.INFO)
 end, { range = true, desc = "Convert buffer/selection to RTF and copy to clipboard" })
