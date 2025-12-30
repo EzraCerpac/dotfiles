3ad40639624acb2481c8d8d11a039f61a5206249
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-30 13:48:14 +0100

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/helpers/to_clipboard.sh

--- dot_config/nvim/lua/helpers/to_clipboard.sh
+++ dot_config/nvim/lua/helpers/to_clipboard.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Reads from stdin, converts to RTF via pygmentize, copies to macOS clipboard
-# Usage: echo "code" | to_clipboard.sh <filetype> [theme]
-# Defaults: filetype=lua, theme=xcode
+# Converts file to RTF via pygmentize and copies to macOS clipboard
+# Usage: to_clipboard.sh <filetype> [theme] <input_file>
 
 LEXER="${1:-lua}"
 THEME="${2:-xcode}"
+INPUT="${3:-/dev/stdin}"
 
 # Write RTF to temp file
 RTFFILE=$(mktemp).rtf
-cat | pygmentize -f rtf -O style="$THEME" -l "$LEXER" > "$RTFFILE"
+pygmentize -f rtf -O style="$THEME" -l "$LEXER" "$INPUT" > "$RTFFILE"
 
 # Use AppleScript to copy only RTF (no plain text)
 osascript << APPLESCRIPT
