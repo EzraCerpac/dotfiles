1f8b2ea460789853083ad64c5fd8b8c53f726af6
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-30 11:49:28 +0100

    Update .config/nvim/lua/plugins/disabled.lua

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- Temporarily disable experimental Copilot LSP while debugging NVIM 0.12-dev LSP issues
   { "copilotlsp-nvim/copilot-lsp", enabled = false },
 }
