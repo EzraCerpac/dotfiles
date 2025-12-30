3a4731e4ef273c9b78a78fd3e4de17eccf431353
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2025-12-30 13:38:50 +0100

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/explorer.lua

--- dot_config/nvim/lua/plugins/explorer.lua
+++ dot_config/nvim/lua/plugins/explorer.lua
@@ -138,7 +138,7 @@ return {
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
       view_options = {
-        show_hidden = true,
+        -- show_hidden = true,
       },
       keymaps = {
         ["H"] = { "actions.parent", mode = "n" },
