d7d843f0ea1367f4c81a2c0fa96e7778e13ab293
Ezra Cerpac <e.cerpac@icloud.com>
2026-01-25 16:13:48 +0100

    Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/overseer.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -68,6 +68,6 @@ vim.filetype.add({ extension = { wgsl = "wgsl" } })
 -- })
 
 -- LSP Server to use for Python.
-vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
--- vim.lsp.enable("ty")
+vim.lsp.enable("ty")
