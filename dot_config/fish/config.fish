decfc66943a6ab8743ecec113db731cdefc38e6f
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2026-01-26 20:39:50 +0100

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -59,8 +59,7 @@ alias vl='nvim-local'
 
 fzf --fish | source # Set up fzf key bindings
 
-alias claude='JJ_CONFIG=~/.config/jj/claude-config.toml claude --dangerously-skip-permissions'
-alias cc='claude'
+alias cc='JJ_CONFIG=~/.config/jj/claude-config.toml claude --dangerously-skip-permissions'
 
 # ---------- Tailscale ----------
 alias tailscale='/Applications/Tailscale.app/Contents/MacOS/Tailscale'
