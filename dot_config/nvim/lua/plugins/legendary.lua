5d7769ffd7042f6344b59a7d494c46c557700b8e
Ezra Cerpac <e.cerpac@icloud.com>
2026-01-26 21:10:40 +0100

    Update .config/nvim/lua/plugins/legendary.lua

--- dot_config/nvim/lua/plugins/legendary.lua
+++ dot_config/nvim/lua/plugins/legendary.lua
@@ -6,5 +6,25 @@ return {
   priority = 10000,
   lazy = false,
   -- sqlite is only needed if you want to use frecency sorting
-  -- dependencies = { 'kkharji/sqlite.lua' }
+  dependencies = { "kkharji/sqlite.lua" },
+  keys = {
+    { "<C-p>", "<cmd>Legendary<cr>", desc = "Legendary Keymaps" },
+  },
+  config = function()
+    require("legendary").setup({
+      extensions = {
+        lazy_nvim = true,
+        smart_splits = {
+          directions = { "h", "j", "k", "l" },
+          mods = {
+            move = "<M>",
+            resize = "<C-M>",
+          },
+        },
+      },
+      which_key = {
+        auto_register = true,
+      },
+    })
+  end,
 }
