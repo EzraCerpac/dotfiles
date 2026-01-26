e72bc6b0156dbae53a39fec1339b4d3f7e465a2c
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2026-01-26 21:04:50 +0100

    Add .config/nvim/lua/plugins/legendary.lua

---
+++ dot_config/nvim/lua/plugins/legendary.lua
@@ -0,0 +1,10 @@
+return {
+  -- https://github.com/mrjones2014/legendary.nvim
+  "mrjones2014/legendary.nvim",
+  -- since legendary.nvim handles all your keymaps/commands,
+  -- its recommended to load legendary.nvim before other plugins
+  priority = 10000,
+  lazy = false,
+  -- sqlite is only needed if you want to use frecency sorting
+  -- dependencies = { 'kkharji/sqlite.lua' }
+}
