c6841a9cb0f1bd9678941cabbc3c4914d4bcd8c8
Ezra Cerpac <e.cerpac@icloud.com>
2026-02-08 12:29:09 +0100

    Add .codex/environments/environment.toml Update .codex/config.toml Remove .config/fish/conf.d/00_fig_pre.fish Remove .config/fish/conf.d/99_fig_post.fish Update .config/fish/config.fish Update .config/nvim/lua/plugins/ai.lua Update .config/nvim/lua/plugins/conform.lua Update .config/nvim/lua/plugins/git.lua Update .config/starship.toml Add .julia/environments/v1.12/Manifest.toml Add .julia/environments/v1.12/Project.toml Change attributes of .config/jj/agent-config.toml

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,5 +1,17 @@
 return {
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500, -- ms before showing blame
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
+    },
+  },
+  {
     "kdheepak/lazygit.nvim",
     enabled = false,
   },
