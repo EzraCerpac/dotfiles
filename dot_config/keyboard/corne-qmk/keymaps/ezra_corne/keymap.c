803a121d8166ed633b5bd023726fd0d4a8f28b62
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2026-03-16 12:03:21 +0100

    Update .config/kanata/config.kbd Update .config/keyboard/corne-qmk/keymaps/ezra_corne/keymap.c Add .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/disabled.lua Add .config/nvim/lua/plugins/start.lua Update 04-setup-macos.sh

--- dot_config/keyboard/corne-qmk/keymaps/ezra_corne/keymap.c
+++ dot_config/keyboard/corne-qmk/keymaps/ezra_corne/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PGUP,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PGDN,
-                                   KC_LALT, KC_ENT,  KC_BSPC, MO(_NAV_FN), KC_SPC, KC_DEL
+                                   KC_LCTL, KC_LSFT, KC_SPC,  TH_NAV_BSPC, KC_RALT, MT(MOD_RGUI, KC_DEL)
     )
 };
 
