e4499b751bd6a26717aa1cee44b4b86bdbb4b9ca
Ezra Cerpac <47384389+EzraCerpac@users.noreply.github.com>
2026-03-17 10:43:03 +0100

    Update bin/jw Update .config/chezmoi/smart-apply.toml Update .config/fish/completions/jw.fish Update .config/fish/completions/jwc.fish Update .config/fish/fish_variables

--- dot_config/fish/completions/jwc.fish
+++ dot_config/fish/completions/jwc.fish
@@ -2,5 +2,15 @@ function __jwc_workspaces
     jj workspace list -T 'name ++ "\n"' --color=never 2>/dev/null
 end
 
+function __jwc_workspace_candidates
+    for name in (__jwc_workspaces)
+        printf '%s\t%s\n' $name 'Existing workspace'
+    end
+
+    printf '%s\t%s\n' @ 'Current workspace'
+    printf '%s\t%s\n' - 'Previous workspace'
+    printf '%s\t%s\n' ^ 'Default workspace'
+end
+
 complete -e -c jwc
-complete -c jwc -f -a '(__jwc_workspaces) - @ ^' -d 'Workspace name'
+complete -c jwc -f -a '(__jwc_workspace_candidates)'
