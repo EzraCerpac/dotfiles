deac33f4ca59616b7ff2d12b6f232aa5f9a39027
Ezra Cerpac <e.cerpac@icloud.com>
2026-04-08 20:00:14 +0200

    Update .config/nvim/lua/plugins/markdown.lua

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -5,6 +5,15 @@ return {
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
+  config = function(plugin)
+    local app_dir = plugin.dir .. "/app"
+    local has_prebuilt = vim.fn.glob(app_dir .. "/bin/markdown-preview-*") ~= ""
+    local has_node_runtime = vim.fn.isdirectory(app_dir .. "/node_modules/tslib") == 1
+
+    if not has_prebuilt and not has_node_runtime then
+      vim.fn["mkdp#util#install_sync"](true)
+    end
+  end,
   keys = {
     { "<localLeader>m", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview", ft = "markdown" },
   },
